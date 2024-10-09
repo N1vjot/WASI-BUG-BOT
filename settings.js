d72c2eee3337bcfa23fc3a4398b2306fe3ec14fc
N1vjot <socialhelp.co@gmail.com>
2024-10-09 17:19:36 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -3,7 +3,7 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 //contact details
-global.ownernomer = "7986913468"
+global.ownernomer = "923347748038"
 global.ownername = "🐛 MR WASI "
 global.ytname = "YT: Dont have"
 global.socialm = "GitHub: private"
